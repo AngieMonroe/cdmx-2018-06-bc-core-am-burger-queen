8ab0033dbe6870917cb6d5239e911d2598e35ab4
AngieMonroe <ame.monroy@gmail.com>
2018-10-16 08:27:47 -0500

    Updates

---
+++ precache-manifest.eefcefee4b58572ecd0f83b3b492c44a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "db3000449ee3cfd6d8bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/runtime~main.db300044.js"
+  },
+  {
+    "revision": "33e44e1eea811eeb6271",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/main.33e44e1e.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "33e44e1eea811eeb6271",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/main.5cf76de5.chunk.css"
+  },
+  {
+    "revision": "e92e2876a497f8481207df2567dbe60b",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/index.html"
+  }
+];
