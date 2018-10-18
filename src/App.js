3291b35bf088d76d7c99823fab0ca28c201a5e64
AngieMonroe <ame.monroy@gmail.com>
2018-10-18 14:38:22 -0500

    Maquetado Desayuno

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
-import Login from './components/Login';
-
 import './App.css';
+import firebaseConf from './firebaseConf/FirebaseConf';
+import Login from '../src/components/Login';
+import Wall from '../src/components/Wall';
+
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      user: {}
+    }
+  }
+
+  componentDidMount(){
+    this.authListener();
+  }
+
+  authListener() {
+    firebaseConf.auth().onAuthStateChanged((user)=>{
+      if(user){
+        this.setState({ user });
+      } else{
+        this.setState({ user: null });
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Login />
+      {this.state.user ? (<Wall user={this.state.user} />) : (<Login />)}
       </div>
-    );
-  }
+    )}
 }
 
 export default App;
