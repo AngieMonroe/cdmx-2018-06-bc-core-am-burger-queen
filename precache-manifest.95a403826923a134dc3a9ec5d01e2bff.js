378f92427efcdd74bd7cd29a46a1a4750b54dc74
AngieMonroe <ame.monroy@gmail.com>
2018-11-28 17:22:56 -0600

    Updates

--- precache-manifest.95a403826923a134dc3a9ec5d01e2bff.js
+++ precache-manifest.95a403826923a134dc3a9ec5d01e2bff.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "69f8a0617ac472f78e45841323a3df9e",
-    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Light.69f8a061.woff2"
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Light.3b813c2a.woff"
   },
   {
-    "revision": "2fe58ae12c488814ffc3",
-    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/main.ebf37690.chunk.css"
+    "revision": "aa997de627568fa8f924",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/main.aa417e83.chunk.css"
   },
   {
-    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
-    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Bold.e31fcf18.ttf"
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/arrow_right.8ef6a08c.svg"
   },
   {
     "revision": "74039e41869d24fa8876",
@@ -28,12 +28,12 @@ self.__precacheManifest = [
     "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
-    "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/fontawesome-webfont.fee66e71.woff"
-  },
-  {
     "revision": "af7ae505a9eed503f8b8e6982036873e",
     "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
@@ -56,20 +56,20 @@ self.__precacheManifest = [
     "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Thin.7500519d.woff"
   },
   {
-    "revision": "94998475f6aea65f558494802416c1cf",
-    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Thin.94998475.ttf"
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Light.69f8a061.woff2"
   },
   {
     "revision": "a990f611f2305dc12965f186c2ef2690",
     "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Light.a990f611.eot"
   },
   {
-    "revision": "3b813c2ae0d04909a33a18d792912ee7",
-    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Light.3b813c2a.woff"
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Thin.94998475.ttf"
   },
   {
-    "revision": "2fe58ae12c488814ffc3",
-    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/main.2fe58ae1.chunk.js"
+    "revision": "aa997de627568fa8f924",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/main.aa997de6.chunk.js"
   },
   {
     "revision": "46e48ce0628835f68a7369d0254e4283",
@@ -84,12 +84,12 @@ self.__precacheManifest = [
     "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Regular.2751ee43.woff2"
   },
   {
-    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
-    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Regular.ba3dcd89.woff"
-  },
-  {
     "revision": "df7b648ce5356ea1ebce435b3459fd60",
     "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Regular.ba3dcd89.woff"
   },
   {
     "revision": "574fd0b50367f886d359e8264938fc37",
@@ -108,31 +108,31 @@ self.__precacheManifest = [
     "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Bold.ecdd509c.eot"
   },
   {
-    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
-    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Bold.39b2c303.woff2"
-  },
-  {
     "revision": "894a2ede85a483bf9bedefd4db45cdb9",
     "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Bold.39b2c303.woff2"
   },
   {
     "revision": "dc81817def276b4f21395f7ea5e88dcd",
     "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Bold.dc81817d.woff"
   },
   {
-    "revision": "8ef6a08cdc1154920165680a4edde771",
-    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/arrow_right.8ef6a08c.svg"
-  },
-  {
     "revision": "11911410dca2de148f30954eb2fd5eab",
     "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/Roboto-Bold.e31fcf18.ttf"
   },
   {
     "revision": "74039e41869d24fa8876",
     "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/1.eb34393d.chunk.css"
   },
   {
-    "revision": "98ebe10a480e0747d4772fe21d7672fc",
+    "revision": "c67b724da4371e6103865bd008a710fa",
     "url": "/cdmx-2018-06-bc-core-am-burger-queen/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/cdmx-2018-06-bc-core-am-burger-queen/precache-manifest.3cb7010bf174506a5490cf948b1c1253.js"
+  "/cdmx-2018-06-bc-core-am-burger-queen/precache-manifest.95a403826923a134dc3a9ec5d01e2bff.js"
 );
 
 workbox.clientsClaim();
